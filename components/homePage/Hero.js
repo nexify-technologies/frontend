afd47dc59ac72ac834cca66e4547f0fea5ad0f1f
Partha Paul <pparthapaul11@gmail.com>
2022-12-04 21:22:57 +0530

    starting

---
+++ components/homePage/Hero.js
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+
+import classes from './hero.module.css';
+
+const Hero = () => {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/partha.jpg"
+          alt="An image showing Max"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Partha</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        Angular or React.
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+import FeaturedPosts from '../components/homePage/FeaturedPosts';
+import Hero from '../components/homePage/Hero';
+import { getFeaturedPosts } from '../lib/postsUtil';
+
+const HomePage = props => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Partha' Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development"
+        />
+      </Head>
+      <Hero />
+      <FeaturedPosts posts={props.posts} />
+    </Fragment>
+  );
+};
+
+export const getStaticProps = () => {
+  const featuredPosts = getFeaturedPosts();
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+    revalidate: 60,
+  };
+};
+
+export default HomePage;
