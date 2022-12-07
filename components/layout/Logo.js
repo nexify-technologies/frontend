bb777fbb4f0e2601acedd9e757544787d3df9452
Partha Paul <pparthapaul11@gmail.com>
2022-12-07 21:27:52 +0530

    test deployment

--- components/layout/Logo.js
+++ components/layout/Logo.js
@@ -1,7 +1,7 @@
 import classes from './logo.module.css';
 
 const Logo = () => {
-  return <div className={classes.logo}>Partha' Next Blog</div>;
+  return <div className={classes.logo}>New Startup</div>;
 };
 
 export default Logo;
