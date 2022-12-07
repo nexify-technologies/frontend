bc08589126cca0a6cc22bd60464e9c975f5cbff4
Partha Paul <pparthapaul11@gmail.com>
2022-12-07 22:00:17 +0530

    yarn problem

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 git pull origin main
-npm install yarn -g 
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
 yarn install
 yarn build 
 yarn export
-sudo service nginx reload 
+echo "paulteli" | sudo -S service nginx reload 
