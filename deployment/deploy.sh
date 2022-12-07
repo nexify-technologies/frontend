2e052245eea79f30212c6348b7cd219b2ce586ee
Partha Paul <pparthapaul11@gmail.com>
2022-12-07 22:16:22 +0530

    test actions

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 git pull origin main
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
 yarn install
 yarn build 
 yarn export
-echo "paulteli" | sudo -S service nginx reload 
+cat ~/password.txt | sudo -S service nginx reload 
